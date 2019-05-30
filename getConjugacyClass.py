da08b9c299845e1df82345ce90bb853293b790ac
harshpatel <patel-h91@webmail.uwinnipeg.ca>
2019-05-30 11:44:19 -0500

    Additional methods have been added to create in_line notation from cycle notation, finding u-d pattern in in_line permutation and keeping count of each type of pattern. Feature to print result into a file and has been added. All the added features has been tested by some regular inputs.

--- getConjugacyClass.py
+++ getConjugacyClass.py
@@ -4,9 +4,11 @@ import operation
 a = sys.argv
 partition = []
 
+output = 'pattern'
+
 for i in range(1, len(a)-1):
     partition.append(int(a[i]))
 
 f = open(a[len(a)-1], 'wt')
 
-operation.get_conj_class(partition, f)
+operation.get_conj_class(partition, f, output)
--- operation.py
+++ operation.py
@@ -1,32 +1,37 @@
-def get_conj_class(partition, f):
+def get_conj_class(partition, f, output):
     n = sum(partition)
     partition.sort()
     cyc = get_list_structure(partition)
     used = [False]*n
-    create_con_class(cyc, used, 0, f)
+    create_con_class(cyc, used, 0, f, output)
+    if output == 'pattern':
+        write_pattern_file(f)
 
 
 def get_list_structure(partition):
     cyc = []
     if len(partition) == 0:
         return cyc
-    cyc.append([[None] * partition[0]])
+    cyc.append([[None]*partition[0]])
     index = 0
     for i in range(1, len(partition)):
         if partition[i] == partition[i - 1]:
-            cyc[index].append([None] * partition[i])
+            cyc[index].append([None]*partition[i])
         else:
             index += 1
             cyc.append([[None]*partition[i]])
     return cyc
 
 
-def create_con_class(cyc, used, count, f, position=None):
+def create_con_class(cyc, used, count, f, output, position=None):
     obj = get_next_slot(position, cyc)
     no_empty_spot, position = obj['no_more_spot'], obj['array']
     if no_empty_spot:
         count += 1
-        write_in_file(cyc, count, f)
+        if output in switch:
+            switch[output](len(used), cyc, count, f)
+        else:
+            write_cycle_file(len(used), cyc, count, f)
         return count
     x, y, z = position[0], position[1], position[2]
     start, end = 0, len(used)
@@ -38,15 +43,15 @@ def create_con_class(cyc, used, count, f, position=None):
         if not used[a]:
             if z == 0:
                 if y == 0:
-                    obj = rec_call(cyc, used, count, position, x, y, z, a, f)
+                    obj = rec_call(cyc, used, count, position, output, x, y, z, a, f)
                     count, position = obj['count'], obj['position']
                 else:
                     if a+1 > cyc[x][y-1][0]:
-                        obj = rec_call(cyc, used, count, position, x, y, z, a, f)
+                        obj = rec_call(cyc, used, count, position, output, x, y, z, a, f)
                         count, position = obj['count'], obj['position']
             else:
                 if a+1 > cyc[x][y][0]:
-                    obj = rec_call(cyc, used, count, position, x, y, z, a, f)
+                    obj = rec_call(cyc, used, count, position, output, x, y, z, a, f)
                     count, position = obj['count'], obj['position']
     return count
 
@@ -68,21 +73,66 @@ def get_next_slot(array, cyc):
     return {'array': None, 'no_more_spot': True}
 
 
-def rec_call(cyc, used, count, position, x, y, z, a, f):
+def rec_call(cyc, used, count, position, output, x, y, z, a, f):
     cyc[x][y][z] = a + 1
     used[a] = True
-    count = create_con_class(cyc, used, count, f, position)
+    count = create_con_class(cyc, used, count, f, output, position)
     position = [x, y, z]
     cyc[x][y][z] = None
     used[a] = False
     return {'position': position, 'count': count}
 
 
-def write_in_file(cyc, count, f):
-    # f.write(str(count)+'\n')
+def write_cycle_file(n, cyc, count, f):
+    f.write(str(count)+'\n')
     s = ''
     for block in cyc:
         for cycle in block:
             s += str(cycle) + ' '
     f.write(s+'\n')
-    
+
+
+def write_inline_file(n, cyc, count, f):
+    f.write(str(count)+'\n')
+    inline = to_inline(n, cyc)
+    f.write(str(inline)+'\n')
+
+
+def to_inline(n, cyc):
+    inline = [None] * n
+    for block in cyc:
+        for cycle in block:
+            for i in range(len(cycle)):
+                inline[cycle[i] - 1] = cycle[(i + 1) % len(cycle)]
+    return inline
+
+
+def pattern_frequency(n, cyc, count=None, f=None):
+    if n < 2:
+        return
+    inline = to_inline(n, cyc)
+    s = ''
+    for i in range(1, n):
+        if inline[i] > inline[i-1]:
+            s += 'u'
+        else:
+            s += 'd'
+    if s in pattern:
+        pattern[s] += 1
+    else:
+        pattern[s] = 1
+
+
+pattern = {}
+
+
+def write_pattern_file(f):
+    for seq in sorted(pattern.keys()):
+        f.write(str(seq) + '  :  ' + str(pattern[seq]) + '\n')
+
+
+switch = {
+    'inline': write_inline_file,
+    'cycle': write_cycle_file,
+    'pattern': pattern_frequency
+}
